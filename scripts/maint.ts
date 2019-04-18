25afa5a886394e7dc1da0c26f5bf83d4003de148
EVAN Kelly <kelly7682@students.cptc.edu>
2019-04-18 15:16:10 -0700

    Basic prototype of studnet registration web page

---
+++ scripts/maint.ts
@@ -0,0 +1,41 @@
+/**
+ * Represents a CPTC Student 
+ */
+class Student{
+    firstName:string;
+    lastName:string;
+    dateOfBirth:Date;
+}
+
+//test code
+let testStu:Student = new Student();
+testStu.firstName = "Joe"; 
+testStu.lastName = "Ortiz";
+//March 1st 1980
+testStu.dateOfBirth = new Date(1980, 3 ,1);
+
+window.onload = function(){
+    let regBtn = <HTMLElement>
+        document.querySelector("main > button")
+    regBtn.onclick = registerStudent;
+}
+
+function registerStudent(){
+    let nextStudent:Student = getStudent();
+    displayStudent(nextStudent);
+}
+
+function getStudent():Student{
+    let s = new Student();
+    s.firstName = (<HTMLInputElement>
+        document.getElementById("first-name")).value;
+
+        s.lastName = (<HTMLInputElement>
+            document.getElementById("last-name")).value;
+        //TODO: get date of birth
+    return s;
+}
+
+function displayStudent(stu:Student){
+alert(stu.firstName + " " + stu.lastName);
+}
